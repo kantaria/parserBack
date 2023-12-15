0d4bec49aa9ca0b0d1782f73acf8d0a5a06b8663
kantariamo <kantariamo@yandex.ru>
2023-12-15 21:11:44 +0800

    rebuild ssl location

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const https = require('https');
-const sslConfig = require('./sslConfig');
 const parseAndSaveRoute = require('./routes/parseAndSave');
 const getDataRoute = require('./routes/getData');
 
@@ -14,7 +13,16 @@ app.use(cors(), bodyParser.json());
 app.use(parseAndSaveRoute);
 app.use(getDataRoute);
 
-const httpsServer = https.createServer(sslConfig, app);
+
+
+// Пути к вашему ключу и сертификату
+const privateKey = fs.readFileSync('../../../etc/letsencrypt/archive/parser.codetime.am/privkey1.pem', 'utf8');
+const certificate = fs.readFileSync('../../../etc/letsencrypt/archive/parser.codetime.am/cert1.pem', 'utf8');
+
+const credentials = { key: privateKey, cert: certificate };
+
+// Создаем HTTPS сервер, используя приложение Express и учетные данные SSL
+const httpsServer = https.createServer(credentials, app);
 httpsServer.listen(port, () => {
     console.log(`Server listening at https://parser.codetime.am:${port}`);
 });
