132794b291fdcc33b6d14e3f0c01a01e283abfa5
kantariamo <kantariamo@yandex.ru>
2023-12-15 14:35:42 +0800

    Generate html fail with table & add there place new incoming content

--- server.js
+++ server.js
@@ -17,14 +17,60 @@ app.use(bodyParser.json());
 
 app.post('/save-data', (req, res) => {
     const data = req.body.data;
-    fs.writeFile('done.txt', data, (err) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).send('Error writing to file');
+    const tableRow = `<tr><td>${data.webSiteLinks}</td><td>${data.currentUrl}</td><td>${data.currentTime}</td><td>${data.companyTitle}</td></tr>\n`;
+
+    // Проверка существует ли файл
+    fs.exists('data.html', exists => {
+        if (exists) {
+            // Добавляем только строку таблицы
+            fs.appendFile('data.html', tableRow, (err) => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).send('Error writing to file');
+                }
+                res.send('Data added to table');
+            });
+        } else {
+            // Создаем файл с начальной структурой HTML и таблицы, затем добавляем строку
+            const initialHtml = `
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Data Table</title>
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
         }
-        res.send('Data saved to file');
+        th, td {
+            padding: 5px;
+            text-align: left;
+        }
+    </style>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>Web Site Links</th>
+            <th>Current URL</th>
+            <th>Current Time</th>
+            <th>Company Title</th>
+        </tr>
+        ${tableRow}
+    </table>
+</body>
+</html>`;
+            fs.writeFile('data.html', initialHtml, (err) => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).send('Error creating file');
+                }
+                res.send('Table created and data added');
+            });
+        }
     });
 });
+
 
 // Создаем HTTPS сервер, используя приложение Express и учетные данные SSL
 const httpsServer = https.createServer(credentials, app);
